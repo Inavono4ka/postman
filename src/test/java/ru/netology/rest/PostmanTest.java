0b88e4d1d9a1c1ca9877a1362aa6fb61de7cf9bb
Mariia <mariya.cheaptrip@gmail.com>
2022-09-24 23:59:41 +0300

    correct

--- src/test/java/ru/netology/rest/PostmanTest.java
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -1,23 +1,26 @@
 package ru.netology.rest;
 
-
-import org.testng.annotations.Test;
-
+import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanTest {
+
     @Test
-    void shouldReturnDemoAccounts() {
+    void shouldPost() {
         // Given - When - Then
+        // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data")
+                .body("Postman") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
                 .when()
                 .post("/post")
+// Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("Postman"))
         ;
     }
+
 }
